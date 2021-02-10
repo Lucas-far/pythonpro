c2218b6156f3eb693856aad0745d7556143d66bc
Lucas-far <mizoguchia@hotmail.com>
2021-02-10 09:13:17 -0300

    1 - .travis.yml (directories excluded from analysis) 2 - tests.py (pep8 2 line indent inserted)

--- tests/tests.py
+++ tests/tests.py
@@ -3,12 +3,15 @@
 def make_dict(chave, valor):
     return {chave: valor}
 
+
 def test_make_dict():
     assert make_dict('chave', 'valor') == {'chave': 'valor'}
+
 
 def turn_list_into_str(list_items: list):
     """"""
     return "".join(list_items)
+
 
 def test_turn_list_into_str():
     assert turn_list_into_str(['123']) == '123'
